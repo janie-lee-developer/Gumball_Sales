6304937eede11ce57eb65fbe901160d0707c91fe
Janie Lee <janie.lee.inquire@gmail.com>
2020-01-07 00:56:12 -0500

    .parse the JSON data
    1. 'request.responseText' the '.responseText' property retrieves the JSON data in string.
    2. In order to break down the string JSON data into a readable objects inside an array, we need to use .parse method.
    3. Updated the 'updateSales' function, what to do after the JSON data is parsed.

--- gumballSalesDataByAJAX.js
+++ gumballSalesDataByAJAX.js
@@ -1,12 +1,12 @@
 //note: AJAX, XMLHttpRequest constructor only works if the data is stored in same server as the website.
 
 window.onload = function() {
-   var url = "http://localhost/sales.json";
+   var url = "http://localhost/gumball/mightygumball.html";
    var request = new XMLHttpRequest();
    request.open("GET", url);
    request.onload = function() {
      if (request.status == 200) {
-      alert(request.responseText);
+         updateSales(request.responseText);
       }
    };
    request.send(null);
@@ -14,5 +14,17 @@ window.onload = function() {
 
 function updateSales(responseText) {
    var salesDiv = document.getElementById("sales");
-   salesDiv.innerHTML = ResponseText;
+      //calling DOM to get an Element with ID name "sales", so we can replace JSON data into this element.
+   var sales = JSON.parse(responseText);
+      // parsing the JSON data in string.
+   for (var i = 0; i < sales.length; i++) {
+      var sale = sales[i];
+         //individualizing each sale by assigning integer to call each object inside the array.
+      var div = document.createElement("div");
+      div.setAttribute("class", "saleItem");
+         //setting an attribute to the newly created div element, so that way we can give CSS effect later.
+      div.innerHTML = sale.name + " sold " + sale.sales + "gumballs";
+      salesDiv.appendChild(div);
+         //placement of the 'div' element will be a child of 'salesDiv' element (the div element w ID="sales").
+   }
 }
