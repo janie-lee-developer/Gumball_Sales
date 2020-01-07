c63af54e895b2e7003e3d187f6942332861860d2
Janie Lee <janie.lee.inquire@gmail.com>
2020-01-07 01:36:22 -0500

    JSONP with a timer handler
    1. JS code updated.
    2. <script> tag is created by JS.
    3. By .setInterval() method, the handler function is repeated every 3 seconds.
    4. the handler is going to create a new <script> tag, and then replace the <script> tag with a new <script> tag.

--- gumballSalesDataByAJAX.js
+++ gumballSalesDataByAJAX.js
@@ -1,7 +1,33 @@
 //note: AJAX, XMLHttpRequest constructor only works if the data is stored in same server as the website.
 
 window.onload = function() {
-  //Emptyout the AJAX: XMLHttpRequest codes. Instead we are using JSONP for JSON data at different server.
+  setInterval(handleRefresh, 3000);
+    //the setInterval method will call the 'handleRefresh' function in every 3 seconds.
+}
+
+function handleRefresh() {
+  var url = "http://gumball.wickedlysmart.com?callback=updateSales";
+  
+  var newScriptElement = document.createElement("script");
+    //creating a new element <script> at DOM.
+  newScriptElement.setAttribute("src", url);
+    //adding an attribute on <script src="url"
+  newScriptElement.setAttribute("id", "jsonp");
+    //adding an ID attribute called "jsonp" to add CSS later.
+  
+  var oldScriptElement = document.getElementByID("jsonp");
+    //declare a variable and assign an element with ID = "jsonp".
+  var head = document.getElementByTagName("head")[0];
+    //declare a variable and assign <head> tag with array number [0], meaning the first <head> tag in DOM.
+  if (oldScriptElement == null) {
+    head.appendChild(newScriptElement);
+    // if there is no <script> element w/ id="jsonp", add the new <script> tag under <head>. 
+    // This is to make sure, there are no duplicating <script> tags.
+  } else {
+    head.replaceChild(newScriptElement, oldScriptElement);
+  }
+    // If there's already a <script> tag, replace the old <script> tag with the new <script> tag. 
+    // This handleRefresh function is being repeated every 3 seconds.
 }
 
 function updateSales(sales) {
