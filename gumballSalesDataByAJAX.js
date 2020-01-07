f5bae154e210273fd36d5e7fc56ea88c143843cc
Janie Lee <janie.lee.inquire@gmail.com>
2020-01-07 00:40:31 -0500

    Creating Handlers
    1. Adding handler 'window.onload' to handle the AJAX request.
    2. Create another function (which is called inside the AJAX handler) to what to do with the JSON data.

--- gumballSalesDataByAJAX.js
+++ gumballSalesDataByAJAX.js
@@ -1,11 +1,18 @@
 //note: AJAX, XMLHttpRequest constructor only works if the data is stored in same server as the website.
 
-var url = "http://someserver.com/data.json";
-var request = new XMLHttpRequest();
-request.open("GET", url);
-request.onload = function() {
-   if (request.status == 200) {
-     alert(request.responseText);
-   }
- };
- request.send(null);
+window.onload = function() {
+   var url = "http://localhost/sales.json";
+   var request = new XMLHttpRequest();
+   request.open("GET", url);
+   request.onload = function() {
+     if (request.status == 200) {
+      alert(request.responseText);
+      }
+   };
+   request.send(null);
+}
+
+function updateSales(responseText) {
+   var salesDiv = document.getElementById("sales");
+   salesDiv.innerHTML = ResponseText;
+}
