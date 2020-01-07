37657cfc072ccca2ca7af66bac67554a5c81bf58
Janie Lee <janie.lee.inquire@gmail.com>
2020-01-07 00:29:27 -0500

    AJAX XMLHttpRequest Template
    Use if the JSON data is stored in same server as the website.

---
+++ gumballSalesDataByAJAX.js
@@ -0,0 +1,11 @@
+//note: AJAX, XMLHttpRequest constructor only works if the data is stored in same server as the website.
+
+var url = "http://someserver.com/data.json";
+var request = new XMLHttpRequest();
+request.open("GET", url);
+request.onload = function() {
+   if (request.status == 200) {
+     alert("Data received!");
+   }
+ };
+ request.send(null);
